6bdddd52b06468a7af7b0d979622468fecfd610e
ruddra <ruddra90@gmail.com>
2020-05-08 12:51:54 +0000

    deploy: ruddra/ruddra.hugo@18e6330c2d90bce350507179f513b0f4719c5291
    Former-commit-id: 039fd599c49626431c384ae6f480d9e59a57afd0

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
+if (!('IntersectionObserver' in window)) {
+  var script = document.createElement("script");
+  script.src = "/js/intersection-observer.js";
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
 var observer = lozad();
 observer.observe();
 

3fea34ac22740e3a6c985f3bc10713dc3f755ec2
ruddra <ruddra90@gmail.com>
2020-05-30 10:07:17 +0000

    deploy: ruddra/ruddra.hugo@4206d944fe6f9680503d0a0b6c4715816e8b8b08
    Former-commit-id: 930e0993eca3fe3821daf1490c3223e78f26d3b6 [formerly 2f54a91e121004f01cc734f26c45e89b1c43b353]
    Former-commit-id: 51f57ad4be3a199ec2a130618182d0e7b28d4846

--- js/common.js
+++ js/common.js
@@ -1,9 +1,3 @@
-if (!('IntersectionObserver' in window)) {
-  var script = document.createElement("script");
-  script.src = "/js/intersection-observer.js";
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
-
 var observer = lozad();
 observer.observe();
 

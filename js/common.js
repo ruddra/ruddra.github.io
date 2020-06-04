27f4f774ec2431a0f05b02c182c99aad111dc5b6
ruddra <ruddra90@gmail.com>
2020-06-04 07:35:01 +0000

    deploy: ruddra/ruddra.hugo@769b4291f451c3f42a592a2f75563f809be5df16
    Former-commit-id: 9e4899ca58fd1814890b05f3cdbfc749d0b093f1 [formerly 8f7019993a7051e7e36277bf04e899433a44728a]
    Former-commit-id: b6c7ca0adc10a69d84f691a98f1ff5fe365df302

--- js/common.js
+++ js/common.js
@@ -1,31 +1,3 @@
-"use strict";
-function browserSupportsAllFeatures() {
-  return window.IntersectionObserver && window.URLSearchParams;
-}
-
-function loadScript() {
-  var js = document.createElement('script');
-  js.src = "https://polyfill.io/v3/polyfill.min.js?flags=gated&features=Object.assign%2CIntersectionObserver%2CURLSearchParams";
-  js.onload = function () {
-    loadLozad();
-  };
-  js.onerror = function () {
-    console.log("Error")
-  };
-  document.head.appendChild(js);
-}
-
-function loadLozad() {
-  var observer = lozad();
-  observer.observe();
-}
-
-if (!browserSupportsAllFeatures) {
-  loadScript()
-} else {
-  loadLozad();
-}
-
 if (window.location.hash == "#comment-submitted") {
   var $comTarget = document.getElementById('comment-submitted-box');
   $comTarget.style.display = 'block';
@@ -140,24 +112,6 @@ function toggleMarkDownTable() {
   }
 }
 
-function showPrivacy() {
-  setTimeout(function () {
-    if (localStorage.getItem("cookieSeen") != "shown") {
-      localStorage.setItem("cookieSeen", "shown");
-      var x = document.getElementById("snackbar");
-      x.className = "show";
-    }
-    (function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-58095062-1', 'auto');
-    ga('send', 'pageview');
-  }, 5000)
-}
-showPrivacy();
 
 function closePrivacy() {
   var x = document.getElementById("snackbar");
---
+++ js/important.js
@@ -0,0 +1,47 @@
+"use strict";
+function browserSupportsAllFeatures() {
+  return window.IntersectionObserver && window.URLSearchParams;
+}
+
+function loadScript() {
+  var js = document.createElement('script');
+  js.src = "https://polyfill.io/v3/polyfill.min.js?flags=gated&features=Object.assign%2CIntersectionObserver%2CURLSearchParams";
+  js.onload = function () {
+    loadLozad();
+  };
+  js.onerror = function () {
+    console.log("Error")
+  };
+  document.head.appendChild(js);
+}
+
+function loadLozad() {
+  var observer = lozad();
+  observer.observe();
+}
+
+if (!browserSupportsAllFeatures) {
+  loadScript()
+} else {
+  loadLozad();
+}
+
+
+function showPrivacy() {
+  setTimeout(function () {
+    if (localStorage.getItem("cookieSeen") != "shown") {
+      localStorage.setItem("cookieSeen", "shown");
+      var x = document.getElementById("snackbar");
+      x.className = "show";
+    }
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-58095062-1', 'auto');
+    ga('send', 'pageview');
+  }, 5000)
+}
+showPrivacy();

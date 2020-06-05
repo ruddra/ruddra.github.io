9eb7e35750533da7a04687c75613af2b258c88eb
ruddra <ruddra90@gmail.com>
2020-06-05 14:13:33 +0000

    deploy: ruddra/ruddra.hugo@e215f5d3d8dcaa532b4e6bf3bc3bc9772376faea
    Former-commit-id: 20b9dc36b83db35b3047d13589c2ed7e6e0ab17b [formerly 0dd8614ff534611fa60480ca20c3a8db889aa41f]
    Former-commit-id: fc3dcabbc13fabbce080f72d511ae7521034d8cf

--- js/load-lozad.js
+++ js/load-lozad.js
@@ -1,8 +1,1 @@
-"use strict";
-if (!('IntersectionObserver' in window)) {
-  var script = document.createElement("script");
-  script.src = "/js/intersection-observer.js";
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
-var observer = lozad();
-observer.observe();
+"use strict";if(!("IntersectionObserver"in window)){var script=document.createElement("script");script.src="/js/intersection-observer.js",document.getElementsByTagName("head")[0].appendChild(script)}var observer=lozad();observer.observe();
